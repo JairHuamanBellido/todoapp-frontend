74e9ae3be52169b751f1781043c73141e52ea312
Jair Huaman <jair.huaman@reign.cl>
2022-10-01 11:00:30 -0500

    Add styled components

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   swcMinify: true,
-}
+  compiler: { styledComponents: { ssr: true } },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
